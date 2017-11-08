8781973f775937b4da0bde0a7346ed8313317f5f
Akseli Kuistio <akuistio@gmail.com>
2017-11-08 15:03:36 +0200

    cleanup

--- src/decorator/Text.java
+++ src/decorator/Text.java
@@ -1,17 +1,9 @@
 
 package decorator;
 
-import java.io.File;
-
 public class Text implements TextIF {
-    private File file;
     private String text;
     
-    
-    public Text() {
-        //this.file = new File(path);
-    }
-
     @Override
     public void write(String str) {
         this.text = str;
